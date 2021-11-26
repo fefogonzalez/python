8be385d7ace00e57b694bc87f2dbab48409ed85d
Federico Gonzalez <42825387+fefogonzalez@users.noreply.github.com>
2021-11-26 01:14:09 -0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,7 +1,23 @@
 import os
 import platform
 import psutil #instalar previamente con -->  "pip install psutil"
+import paramiko #instalar previamente con --> pip install paramiko
+import socket
 
+def Verificar_disponibilidad(ip,port):
+    #Ejemplo tomado de https://www.youtube.com/watch?v=HQR67_suo-E
+    socket.setdefaulttimeout(.5)
+
+    DEVICE_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result_of_check = DEVICE_SOCKET.connect_ex((ip,port))
+
+    if result_of_check == 0:
+       Disponible = 1
+       DEVICE_SOCKET.close()
+    else:
+       Disponible = 0
+    
+    return (Disponible)
 
 def getListOfProcessActive():
     print('*** Create a list of all running processes ***')
@@ -39,6 +55,38 @@ def getLoggedUsers():
     return ('')
 
 
+def ObtenerProcesosActivos():
+    host = "IP-ADDR"
+    port = 22
+    username = "USUARIO"
+    password = "PASSWORD"
+
+    command = "pwd"
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port, username, password)
+
+    stdin, stdout, stderr = ssh.exec_command(command)
+    lines = stdout.readlines()
+    print(lines)
+
+def ObtenerUsuariosConectados():
+    host = "IP-ADDR"
+    port = 22
+    username = "USUARIO"
+    password = "PASSWORD"
+
+    command = "who"
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port, username, password)
+
+    stdin, stdout, stderr = ssh.exec_command(command)
+    lines = stdout.readlines()
+    print(lines)
+
 
 
 def main():
@@ -47,19 +95,17 @@ def main():
     lista_equipos = inventario.readlines()
  
     count = 0
-    # Strips the newline character
     for equipo in lista_equipos:
         count += 1
-        #MyServer 
-        print(equipo.strip())
 
-        HOST_UP  = True if os.system("ping -c 1 " + equipo.strip()) is 0 else False
-        #print(HOST_UP)
+        Disponibilidad  = Verificar_disponibilidad(equipo.strip(),22)
 
-        if (HOST_UP) :
-            print(getListOfProcessActive())
-            print(getHostInformation())
-            print(getLoggedUsers())
+        if (Disponibilidad) :
+            #print(getListOfProcessActive())
+            #print(getHostInformation())
+            #print(getLoggedUsers())
+            print(ObtenerProcesosActivos())
+            print(ObtenerUsuariosConectados())
         else:
             print("No hay conectividad con el target " + equipo.strip())
 
